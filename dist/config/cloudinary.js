306e7e45923a69682dc647ba7f75f55784eaa2da
Bernard <benbaah104@gmail.com>
2025-04-16 15:23:34 +0000

    update

--- dist/config/cloudinary.js
+++ dist/config/cloudinary.js
@@ -6,7 +6,7 @@ const stream_1 = require("stream");
 // Configure Cloudinary
 cloudinary_1.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_SECRETE,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
 });
--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -4,7 +4,7 @@ import { Readable } from 'stream';
 // Configure Cloudinary
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_SECRETE,
+	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 	secure: true,
 });
